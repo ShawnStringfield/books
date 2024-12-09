21937902d7fe4ae8e37ae9bb5b1ed6daeb4b2dc1
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-12-09 16:22:33 -0500

    refactored user menu into nav component

--- app/components/menus/UserMenu.tsx
+++ app/components/menus/UserMenu.tsx
@@ -1,8 +1,14 @@
 'use client';
 
 import { FC } from 'react';
-import { Button } from '@/app/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/app/components/ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from '@/app/components/ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/app/components/ui/avatar';
 import { useAuth } from '@/lib/auth/hooks/useAuth';
 
@@ -11,21 +17,24 @@ export const UserMenu: FC = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-16 w-16 rounded-full">
-          <Avatar className="h-16 w-16">
-            <AvatarImage src={user?.image ?? ''} alt={user?.name ?? 'User avatar'} />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </Button>
+      <DropdownMenuTrigger className="focus:outline-none cursor-pointer" asChild>
+        <Avatar className="h-8 w-8">
+          <AvatarImage src={user?.image ?? ''} alt={user?.name ?? 'User avatar'} />
+          <AvatarFallback>U</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {user && (
-          <>
-            <DropdownMenuItem className="font-medium">{user.name}</DropdownMenuItem>
-            <DropdownMenuItem onClick={signOut}>Sign out</DropdownMenuItem>
-          </>
-        )}
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium">{user?.name}</p>
+            <p className="text-xs text-gray-500">{user?.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={signOut}>Sign out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
